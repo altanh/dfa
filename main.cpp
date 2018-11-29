140e8533bb1d7896d889758a2ba9a26bb1b68ebb
Altan Haan <altanh@cs.washington.edu>
2018-11-28 20:26:03 -0800

    updated main

--- main.cpp
+++ main.cpp
@@ -1,42 +1,26 @@
+#include <iostream>
+#include <iomanip>
+
 #include "dfa.h"
 
+void usage(const char *prog) {
+  std::cerr << "usage: " << prog << " dfa string" << std::endl;
+}
+
 int main(int argc, char **argv) {
-    DFA a;
-    DFA b;
+  if (argc != 3) {
+    usage(argv[0]);
+    return -1;
+  }
 
-    if(!(a.fromFile("dfas/two_ones.txt") && b.fromFile("dfas/most_two_zeros.txt"))) {
-        std::cerr << "wtf" << std::endl;
+  DFA dfa;
 
-        return -1;
-    }
+  if (!dfa.fromFile(std::string(argv[1]))) {
+    std::cerr << "could not open " << argv[1] << std::endl;
+    return -1;
+  }
 
-    a.print();
-    b.print();
+  std::cout << std::boolalpha << dfa.run(std::string(argv[2])) << std::endl;
 
-    DFA both = DFA::fromProduct(a, b, false);
-
-    both.print();
-
-    both.saveToFile("dfas/both.txt");
-
-    DFA min = both.minimize(true);
-
-    min.print();
-
-    min.saveToFile("dfas/min.txt");
-
-    std::string tests[] = {
-        "1101", "1111100111", "010100",
-        "1010", "0111111110", "1000001",
-        "0", "", "1", "101", "11", "001", "0001", "00000"
-    };
-
-    for(auto &s : tests) {
-        std::cout << "both(" << s << "): " << (both.run(s) ? "true" : "false")  << std::endl;
-        std::cout << "min(" << s << "): " << (min.run(s) ? "true" : "false")  << std::endl;
-    }
-
-    std::cout << "both = min? " << (both.equivalentTo(min) ? "true" : "false") << std::endl;
-
-    return 0;
+  return 0;
 }
